76965ca6cb66461716a666b4dbf1a4c7f7052afb
Viren Sureja <virensureja3@gmail.com>
2020-10-26 11:27:02 +0530

    solved issue of setInterval in index.js and moved script tag to the end of the body tag

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log("shiva");
+// var Session = require("./algo/algorithm.js");	// for node
+import { Session } from "./algo/algorithm.js";
 
-function printInput() {
-	var n = document.getElementById("initial_Sema").value;
-	console.log(n);
-}
+let obj = new Session(5);
+
+var subBut = document.getElementById("submitButton");
+var semaClass = document.getElementById("semaClass");
+
+subBut.addEventListener("click", () => {
+	var semaVal = parseInt(document.getElementById("initial_Sema").value);
+	semaClass.innerHTML = semaVal;
+
+	var time = setInterval(() => {
+		if (semaVal <= 20) clearInterval(time);
+		semaClass.innerHTML = semaVal--;
+	}, 500);
+});
