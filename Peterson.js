ae2bc761cd6f0d6257cbc8ba9081128da005d508
Jay Rank <rankjay0101@gmail.com>
2020-10-28 18:19:46 +0530

    Updated with CSS transitions

--- Peterson.js
+++ Peterson.js
@@ -10,30 +10,88 @@ function EntrySection(process) {
 	other = 1 - process;
 	interest[process] = true;
 	turn = process;
-	
-	while(interest[other]==true && turn==process)
-	{
-		
+
+	//////////////////
+	if (turn == 1) {
+		P1Move(100)
 	}
-	
+	else {
+		P0Move(100)
+	}
+	/////////////////
+
+	if (interest[other] = true && turn == process) {
+		setTimeout(function () {
+
+		}, 5000);
+	}
+
 	CriticalSection(process)
 }
 
 function CriticalSection(process) {
+
+	////////////////////
+	if (process == 0) {
+		P0Move(200)
+	}
+	else {
+		P1Move(300)
+	}
+	////////////////////
+
+
 	var millisecondsToWait = 2500;
-	setTimeout(function() {
-		// Uncommenty the next line to see how dynamically the algorithm works, NOTE it is an instance of the working and not the working itself.
-		// EntrySection(1-process);
+	setTimeout(function () {
+		if (process == 0) {
+			P1Move(100)
+		}
 	}, millisecondsToWait);
-	
+
 	ExitSection(process)
 }
 
 function ExitSection(process) {
-	interest[process]=false;
-	console.log("Completed Process" + process);
+
+	////////////////////
+	if (process == 0) {
+		P0Move(500)
+		CriticalSection(1 - process)
+	}
+	else {
+		P1Move(500)
+	}
+	////////////////////
+
+	interest[process] = false;
 }
 
+function P0Move(valuetomove1) {
+	var elem1 = document.getElementById("P000");
+	var pos1 = 0;
+	var id1 = setInterval(frame1, 5);
+	function frame1() {
+		if (pos1 == 1000) {
+			clearInterval(id1);
+		} else {
+			pos1++;
+			elem1.style.left = pos1 + "px";
+		}
+	}
+}
+
+function P1Move(valuetomove2) {
+	var elem2 = document.getElementById("P001");
+	var pos2 = 0;
+	var id2 = setInterval(frame2, 5);
+	function frame2() {
+		if (pos2 == 1000) {
+			clearInterval(id2);
+		} else {
+			pos2++;
+			elem2.style.left = pos2 + "px";
+		}
+	}
+}
 
 EntrySection(Process0);
-EntrySection(Process1);
