a0926b79fd274e4b43738f9be2b3b8b6aa1b8c7f
Viren Sureja <virensureja3@gmail.com>
2020-11-03 11:29:36 +0530

    ignore it

--- algo/algorithm.js
+++ algo/algorithm.js
@@ -7,7 +7,12 @@ var semaV = document.getElementById("semaVal");
 var suspendQ = document.getElementById("suspendedQ");
 var criticalQ = document.getElementById("criticalQ");
 var completedQ = document.getElementById("completedQ");
+var stepguide = document.getElementById("stepguide");
 var capaVal = document.getElementById("capaVal");
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 export class Session {
 	constructor(iniSema) {
@@ -15,58 +20,42 @@ export class Session {
 		this.SuspendedQueue = new Queue();
 		this.CompletedQueue = new Queue();
 		this.CriticalQueue = new Queue();
-		this.semaphore = iniSema;
-		this.capacity = iniSema;
+		this.semaphore = parseInt(iniSema);
+		this.capacity = parseInt(iniSema);
 
 		// insert all procedure in readyQueue
 		for (let i = 1; i <= this.semaphore; i++) {
+			stepguide.innerHTML += `<div>-> adding process p${i} in ready queue.</div>`;
 			this.ReadyQueue.enqueue(i);
 		}
 
 		// using methods to show the updation
 		this.updateSemaphore();
-		// setTimeout(function (this) {
-		// 	semaV.innerHTML = this.semaphore;
-		// }, 3000);
-		console.log("before af sema");
-		this.sleep(3000);
-		console.log("before up ready");
-
 		this.updateReadyQ();
-		// setTimeout(function () {
-		// 	this.updateReadyQ();
-		// }, 5000);
-		this.sleep(3000);
-		console.log("before af ready");
-		console.log("before up cap");
-
 		this.updateCapacity();
-		this.sleep(3000);
-		console.log("before af cap");
 
-		// setTimeout(function () {
-		// 	this.updateCapacity();
-		// }, 10000);
-
-		// to start the session
-		// this.start();
+		this.start();
 	}
 
 	// sleep function to delay
-	sleep(milli) {
-		const DateTime = Date.now();
-		let currDateTime = null;
-		do {
-			currDateTime = Date.now();
-		} while (currDateTime - DateTime < milli);
-	}
+	// sleep(milli) {
+	// 	const DateTime = Date.now();
+	// 	let currDateTime = null;
+	// 	do {
+	// 		currDateTime = Date.now();
+	// 	} while (currDateTime - DateTime < milli);
+	// }
 
 	// updating methods
-	updateReadyQ() {
+	async updateReadyQ() {
 		readyQ.innerHTML = this.ReadyQueue.printQueue();
+		await sleep(3000);
 	}
-	updateSemaphore() {
+	async updateSemaphore() {
+		console.log("inside semaphore");
 		semaV.innerHTML = this.semaphore;
+		await sleep(3000);
+		console.log("outside semaphore");
 	}
 	updateCriticalQ() {
 		criticalQ.innerHTML = this.CriticalQueue.printQueue();
@@ -77,22 +66,30 @@ export class Session {
 	updateCompletedQ() {
 		completedQ.innerHTML = this.CompletedQueue.printQueue();
 	}
-	updateCapacity() {
+	async updateCapacity() {
 		capaVal.innerHTML = this.capacity;
+		await sleep(3000);
 	}
 
-	addCapacity(count) {
-		this.capacity += count;
-		for (let i = capacity - count + 1; i <= capacity; i++) {
+	addCapacity() {
+		this.capacity = parseInt(this.capacity + 1);
+		this.semaphore = this.semaphore + 1;
+		let temp = this.capacity;
+		for (let i = temp - 1 + 1; i <= temp; i++) {
+			stepguide.innerHTML += `<div>-> adding process p${i} in ready queue.</div>`;
 			this.ReadyQueue.enqueue(i);
 		}
 		this.updateCapacity();
 		this.updateReadyQ();
+		this.start();
 	}
 
 	down() {
-		console.log("inside down");
+		stepguide.innerHTML += `<div>----> entered down <----</div>`;
 		this.semaphore = this.semaphore - 1;
+		stepguide.innerHTML += `<div>-> semaphore value: ${this.semaphore + 1} -> ${
+			this.semaphore
+		}.</div>`;
 		this.updateSemaphore();
 
 		let temp = this.ReadyQueue.front();
@@ -103,43 +100,54 @@ export class Session {
 			// put process in suspended list from ready queue.
 			this.SuspendedQueue.enqueue(temp);
 			this.updateSuspendedQ();
+			stepguide.innerHTML += `<div>-> process p${temp} exits suspended queue.</div>`;
 		} else {
 			// put process in critical section from ready queue.
 			this.CriticalQueue.enqueue(temp);
 			this.updateCriticalQ();
+			stepguide.innerHTML += `<div>-> process p${temp} entered critical section.</div>`;
 		}
+		let temp2 = this.CriticalQueue.front();
+		this.CriticalQueue.dequeue();
+		stepguide.innerHTML += `<div>-> process p${temp2} exits critical section.</div>`;
+		this.updateCriticalQ();
+		this.CompletedQueue.enqueue(temp2);
+		stepguide.innerHTML += `<div>-> process p${temp2} entered completed queue.</div>`;
+		this.updateCompletedQ();
 	}
 
 	up() {
-		console.log("inside up");
+		stepguide.innerHTML += `<div>----> entered up <----</div>`;
 		this.semaphore = this.semaphore + 1;
+		stepguide.innerHTML += `<div>-> semaphore value ${this.semaphore - 1} -> ${
+			this.semaphore
+		}.</div>`;
 		this.updateSemaphore();
 
 		if (this.semaphore <= 0) {
 			// fetch from suspended queue
 			let temp = this.SuspendedQueue.front();
 			this.SuspendedQueue.dequeue();
+			stepguide.innerHTML += `<div>-> process p${temp} exits suspended queue.</div>`;
 			this.updateSuspendedQ();
 
 			// again queuing in readyQueue.
 			this.ReadyQueue.enqueue(temp);
 			this.updateReadyQ();
+			stepguide.innerHTML += `<div>-> process p${temp} entered ready queue.</div>`;
 		}
 	}
 
 	start() {
 		while (true) {
-			console.log("inside while");
-
 			if (this.ReadyQueue.isEmpty()) {
+				stepguide.innerHTML += `<div>-> ready queue is empty.</div>`;
 				console.log("readyQ empty!");
 				break;
-			} else {
-				this.down();
-				this.up();
 			}
+			while (this.semaphore) this.down();
+			while (this.semaphore < 0) this.up();
 		}
-		console.log("out of while");
 		// can put pop up to show the processes is completed
 	}
 }
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { Session } from "./algo/algorithm.js";
 
 var subBut = document.getElementById("submitButton");
 
-var session; // object of session class.
+// object of session class.
+var session;
 
 subBut.addEventListener("click", () => {
 	let semaVal = document.getElementById("initial_Sema").value;
@@ -12,4 +13,9 @@ subBut.addEventListener("click", () => {
 	else {
 		session = new Session(semaVal);
 	}
+});
+
+var increButton = document.getElementById("increButton");
+increButton.addEventListener("click", () => {
+	session.addCapacity(1);
 });
