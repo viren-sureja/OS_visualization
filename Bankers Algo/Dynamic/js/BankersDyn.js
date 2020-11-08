5076b340dacde3235bcdafc5e7fd9ce96bc89684
Viren Sureja <virensureja3@gmail.com>
2020-11-08 11:49:44 +0530

    added delete,add rows and compute

--- Bankers Algo/Dynamic/js/BankersDyn.js
+++ Bankers Algo/Dynamic/js/BankersDyn.js
@@ -1,27 +1,155 @@
+// var remRow = document.getElementById("AddRow");
+var size = 1;
 
-function removeRow(){
-    var lastRow = $('#myTable tr:last');
-    lastRow.remove()
+function removeRow() {
+	if (size > 0) {
+		document.getElementById("myTable").deleteRow(size);
+		size--;
+	}
 }
-function addRow(){
-    var lastRow = $('#myTable tr:last');
-    var rowCount = $("#myTable tr").length;
-    
-    var newRow = '<tr><td>P'
-    + (rowCount - 1)
-    
-    + '<td><input class="Allocation' + rowCount + '1"/></td>'
-    + '<td><input class="Allocation' + rowCount + '2"/></td>'
-    + '<td><input class="Allocation' + rowCount + '3"/></td>'
-    + '<td><input class="Max' + rowCount + '3"/></td>'
-    + '<td><input class="Max' + rowCount + '3"/></td>'
-    + '<td><input class="Max' + rowCount + '3"/></td>'
-    + '<td><input class="Avail' + rowCount + '3"/></td>'
-    + '<td><input class="Avail' + rowCount + '3"/></td>'
-    + '<td><input class="Avail' + rowCount + '3"/></td></tr>';
-    
-    lastRow.after(newRow);
+function addRow() {
+	size++;
+	let name = "p" + size;
+
+	document.getElementById("myTable").innerHTML += `
+                    <tr id=${name}>
+						<td>${name}</td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+						<td><input value="" /></td>
+					</tr>`;
+}
+function showTableData() {
+	document.getElementById("info").innerHTML = "";
+	var myTab = document.getElementById("myTable").rows;
+
+	for (let i = 1; i < myTab.length; i++) {
+		let myrow = myTab[i];
+		for (let j = 1; j < myrow.cells.length - 3; j++) {
+			let mytd = myrow.cells[j];
+			let currValue = mytd.children[0].value;
+		}
+	}
 }
 
+/* 
+for (i = 1; i < myTab.rows.length; i++) {
+    for (var j = 0; j < 3; j++) {
+    
+    }
+}
+*/
 
-  
+function submitValue() {
+	// $("section").remove();
+	document.getElementById("info").innerHTML = "";
+	var myTab = document.getElementById("myTable").rows;
+
+	let alloc = [];
+	// take values of alloc
+	for (let i = 1; i < myTab.length; i++) {
+		let myrow = myTab[i];
+		let tempList = [];
+		for (let j = 1; j <= 3; j++) {
+			let mytd = myrow.cells[j];
+			let currValue = mytd.children[0].value;
+			tempList.push(currValue);
+		}
+		alloc.push(tempList);
+	}
+
+	let max = [];
+	// take values of max.
+	for (let i = 1; i < myTab.length; i++) {
+		let myrow = myTab[i];
+		let tempList = [];
+		for (let j = 4; j <= 6; j++) {
+			let mytd = myrow.cells[j];
+			let currValue = mytd.children[0].value;
+			tempList.push(currValue);
+		}
+		max.push(tempList);
+	}
+
+	n = size;
+	m = 3;
+	let f = [];
+	for (i = 0; i < n; i++) f[i] = 0;
+	let ans = [];
+	for (i = 0; i < n; i++) ans[i] = -1;
+	let ind = 0;
+
+	// fill the need array.
+	let need = [];
+	for (i = 0; i < n; i++) {
+		need[i] = [];
+		for (j = 0; j < m; j++) {
+			need[i][j] = max[i][j] - alloc[i][j];
+		}
+	}
+
+	// fill the avail array
+	let avail = [
+		myTab[1].cells[7].children[0].value,
+		myTab[1].cells[8].children[0].value,
+		myTab[1].cells[9].children[0].value,
+	];
+
+	let y = 0;
+
+	for (k = 0; k < n; k++) {
+		for (i = 0; i < n; i++) {
+			if (f[i] == 0) {
+				let flag = false;
+				for (j = 0; j < m; j++) {
+					if (need[i][j] > avail[j]) {
+						flag = true;
+
+						break;
+					}
+				}
+
+				if (flag == false) {
+					ans[ind++] = i;
+					for (y = 0; y < m; y++) avail[y] += alloc[i][y];
+					f[i] = 1;
+				}
+			}
+		}
+	}
+
+	function addElement(parentId, elementTag) {
+		// Adds an element to the document
+		var p = document.createElement(parentId);
+		var newElement = document.createTextNode(elementTag);
+		//newElement.setAttribute('id', elementId);
+		//newElement.innerHTML = html;
+		p.appendChild(newElement);
+		document.body.appendChild(p);
+	}
+
+	// for (i = 0; i < n; i++) console.log(ans[i]);
+	// var mastList = [];
+	// var mastString = "";
+	console.log("Following is the SAFE Sequence");
+	for (i = 0; i < n; i++) {
+		if (ans[i] >= 0) {
+			addElement("section", "P" + ans[i]);
+			// $dudeoutput1.append("<i>" + ans[i] + "</i>");
+			console.log(` P${ans[i]} ->`);
+			// mastString += ` P${ans[i]} ->`;
+			// compVar.text(mastString);
+		} else {
+			addElement("section", "deadlock");
+			// console.log("deadlock!");
+			// compVar.text("DEADLOCK");
+			break;
+		}
+	}
+}
